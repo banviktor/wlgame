6fd3b571cb49ed289ffecae22a3f2c9fa967e5b2
Viktor Ban <viktor.ban@gmail.com>
2016-09-30 17:40:40 +0200

    Empty Spring project.

---
+++ src/main/java/com/viktorban/wlgame/WlgameApplication.java
@@ -0,0 +1,12 @@
+package com.viktorban.wlgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlgameApplication.class, args);
+	}
+}
