2369d3bf4ddddf30319e5d074b2715863cd5d0b2
Viktor Ban <viktor.ban@gmail.com>
2016-11-15 12:58:52 +0100

    Make UserController base path aware

--- src/main/java/com/viktorban/wlgame/controller/UserController.java
+++ src/main/java/com/viktorban/wlgame/controller/UserController.java
@@ -2,6 +2,7 @@ package com.viktorban.wlgame.controller;
 
 import com.viktorban.wlgame.model.User;
 import com.viktorban.wlgame.model.UserWrapper;
+import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @see com.viktorban.wlgame.model.User
  */
 @RestController
+@BasePathAwareController
 public class UserController {
 
     /**
@@ -20,7 +22,7 @@ public class UserController {
      *
      * @return The logged in user.
      */
-    @RequestMapping(path = "/api/self", method = RequestMethod.GET)
+    @RequestMapping(path = "/self", method = RequestMethod.GET)
     public User currentUser() {
         UserWrapper userWrapper = (UserWrapper) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return userWrapper.getUser();
