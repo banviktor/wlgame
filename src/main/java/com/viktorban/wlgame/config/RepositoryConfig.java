d4268d753bf05bef88e4afae3db70586fe27ad42
Viktor Ban <viktor.ban@gmail.com>
2016-11-15 11:46:04 +0100

    Expose IDs for User and Language.

---
+++ src/main/java/com/viktorban/wlgame/config/RepositoryConfig.java
@@ -0,0 +1,24 @@
+package com.viktorban.wlgame.config;
+
+import com.viktorban.wlgame.model.Language;
+import com.viktorban.wlgame.model.User;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+/**
+ * Configuration for Spring DATA REST repositories.
+ */
+@Configuration
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(User.class);
+        config.exposeIdsFor(Language.class);
+    }
+
+}
