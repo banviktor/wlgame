39830f90eac54baecb64a9e76b6cac129c331a73
Viktor Ban <viktor.ban@gmail.com>
2016-11-18 13:04:57 +0100

    Don't overcomplicate

--- src/main/java/com/viktorban/wlgame/controller/LanguageController.java
+++ src/main/java/com/viktorban/wlgame/controller/LanguageController.java
@@ -1,10 +1,7 @@
 package com.viktorban.wlgame.controller;
 
-import com.viktorban.wlgame.model.Language;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.hateoas.Resources;
-import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 public class LanguageController {
@@ -31,12 +25,7 @@ public class LanguageController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/api/languages")
     public HttpEntity<?> getLanguages() {
-        List<Language> languages = entityManager.createQuery("SELECT l FROM com.viktorban.wlgame.model.Language l").getResultList();
-        Map<String, String> languageMap = new HashMap<>();
-        for (Language language : languages) {
-            languageMap.put(language.getId(), language.getName());
-        }
-        return new ResponseEntity<>(languageMap, HttpStatus.OK);
+        return new ResponseEntity<>(entityManager.createQuery("SELECT l FROM com.viktorban.wlgame.model.Language l").getResultList(), HttpStatus.OK);
     }
 
 }
