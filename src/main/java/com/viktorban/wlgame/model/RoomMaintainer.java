ec97d7e4f1cbf812e78d43626cc8be54d4a49b6a
Viktor Ban <viktor.ban@gmail.com>
2016-11-19 22:38:21 +0100

    Clean up RoomMaintainer

--- src/main/java/com/viktorban/wlgame/model/RoomMaintainer.java
+++ src/main/java/com/viktorban/wlgame/model/RoomMaintainer.java
@@ -11,6 +11,11 @@ import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Maintains rooms using scheduled tasks.
+ *
+ * @see com.viktorban.wlgame.model.Room
+ */
 @Component
 public class RoomMaintainer {
 
@@ -19,21 +24,28 @@ public class RoomMaintainer {
      */
     private static Log log = LogFactory.getLog(RoomMaintainer.class);
 
+    /**
+     * JPA entity manager.
+     */
     @PersistenceContext
     private EntityManager entityManager;
 
+    /**
+     * Loops through open rooms and closes timed out ones.
+     */
     @Scheduled(fixedDelay = 5000L)
     @Transactional
     public void timeoutRooms() {
         List<Room> rooms = entityManager.createQuery("SELECT r FROM com.viktorban.wlgame.model.Room r WHERE r.state <> 'ENDED'").getResultList();
         Date now = new Date();
         for (Room room : rooms) {
-            if ((room.getState() == Room.RoomState.WAITING_FOR_PLAYERS && now.getTime() > room.getOpened().getTime() + Room.timeoutJoin) ||
-                    (room.getState() == Room.RoomState.WAITING_FOR_WORDS && now.getTime() > room.getJoined().getTime() + Room.timeoutUploadWords) ||
-                    (room.getState() == Room.RoomState.IN_PROGRESS && now.getTime() > room.getStarted().getTime() + Room.timeoutUploadSolutions)) {
+            boolean timeoutJoin = room.getState() == Room.RoomState.WAITING_FOR_PLAYERS && now.getTime() > room.getOpened().getTime() + Room.timeoutJoin;
+            boolean timeoutUploadWords = room.getState() == Room.RoomState.WAITING_FOR_WORDS && now.getTime() > room.getJoined().getTime() + Room.timeoutUploadWords;
+            boolean timeoutUploadSolutions = room.getState() == Room.RoomState.IN_PROGRESS && now.getTime() > room.getStarted().getTime() + Room.timeoutUploadSolutions;
+            if (timeoutJoin || timeoutUploadWords || timeoutUploadSolutions) {
                 room.setState(Room.RoomState.ENDED);
                 room.setTimedOut(true);
-                log.info("Room " + room.getRoomId() + " timed out.");
+                log.info(room.toString() + " timed out.");
             }
         }
         entityManager.flush();
