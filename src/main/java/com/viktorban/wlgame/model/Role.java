8705c702c8dd5f89c784bc4502106617aa7ac9c4
Viktor Ban <viktor.ban@gmail.com>
2016-11-19 22:39:44 +0100

    Clean up Role

--- src/main/java/com/viktorban/wlgame/model/Role.java
+++ src/main/java/com/viktorban/wlgame/model/Role.java
@@ -33,7 +33,7 @@ public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
-   private Long id;
+    private Long id;
 
     /**
      * The role's name.
