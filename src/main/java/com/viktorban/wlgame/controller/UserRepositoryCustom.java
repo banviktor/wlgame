b38ec74a943bd37264368ab03a38b6e812ae708e
Viktor Ban <viktor.ban@gmail.com>
2016-11-19 19:59:50 +0100

    Clean up UserRepositoryCustom

--- src/main/java/com/viktorban/wlgame/controller/UserRepositoryCustom.java
+++ src/main/java/com/viktorban/wlgame/controller/UserRepositoryCustom.java
@@ -3,12 +3,31 @@ package com.viktorban.wlgame.controller;
 import com.viktorban.wlgame.model.User;
 import org.springframework.security.access.prepost.PreAuthorize;
 
+/**
+ * Methods of the User repository that need a custom implementation.
+ *
+ * @see com.viktorban.wlgame.model.User
+ * @see com.viktorban.wlgame.controller.UserRepository
+ * @see com.viktorban.wlgame.controller.UserRepositoryImpl
+ */
 public interface UserRepositoryCustom {
 
+    /**
+     * Saves a new or already existing User.
+     *
+     * @param s The User to insert or update.
+     * @return The saved User.
+     */
     @PreAuthorize("permitAll()")
     <S extends User> S save(S s);
 
+    /**
+     * Returns an existing User.
+     *
+     * @param id The User identifier.
+     * @return The user.
+     */
     @PreAuthorize("permitAll()")
-    User findOne(Long aLong);
+    User findOne(Long id);
 
 }
